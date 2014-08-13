d270eeb020497a765e8aa75b2adbbb64ad1b47fa
Neil Rackett <neil.rackett@mesmotronic.com>
2014-08-13 08:54:50 +0100

    Resolved Collection attributes issue

--- src/conbo/collections/Collection.js
+++ src/conbo/collections/Collection.js
@@ -156,7 +156,7 @@ conbo.Collection = conbo.List.extend
 				
 				if (options.merge) 
 				{
-					existing.set(model.attributes, options);
+					existing.set(model.__attributes__, options);
 					if (sortable && !sort && existing.hasChanged(sortAttr)) sort = true;
 				}
 
