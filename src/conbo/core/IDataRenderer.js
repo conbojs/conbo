adb7c5fdc46a91a0f35350c095af43bdb01789de
Neil Rackett <neil.rackett@mesmotronic.com>
2014-08-13 10:32:55 +0100

    Added IDataRenderer pseudo-interface

---
+++ src/conbo/core/IDataRenderer.js
@@ -0,0 +1,10 @@
+/**
+ * Interface class for data renderers, for example an item renderer for
+ * use with the cb-repeat attribute
+ * 
+ * @author Neil Rackett
+ */
+conbo.IDataRenderer =
+{
+	data: undefined
+}
