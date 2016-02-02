7deae4b8a2d923c99d5f083ceae36d83b4c826ff
Neil Rackett <neil.rackett@mesmotronic.com>
2016-02-02 19:22:35 +0000

    Added nested cb-repeat example

---
+++ examples/RepeatNested.js
@@ -0,0 +1,30 @@
+/**
+ * Example of how to use cb-repeat to display nested arrays
+ * using Conbo.js
+ * 
+ * @author	Neil Rackett
+ */
+conbo('conbo', function(undefined)
+{
+	'use strict'
+	
+	var ns = this;
+	
+	ns.MyApp = conbo.Application.extend
+	({
+		namespace: ns,
+		
+		myList:
+		[
+			{
+				label: 'Boys names',
+				names: ['Aaron','Abdul','Abdullah','Abel','Abraham','Abram','Abriel','Ace','Adam','Adan']
+			},
+			{
+				label: 'Girls names',
+				names: ['Kalea','Kaleigh','Kali','Kalia','Kamala','Kamryn','Kara','Karen','Kari','Karin']
+			}
+		]
+	});
+	
+});
