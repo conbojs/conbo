812163844b3e88b7ca61422f1ae3e37d6ad0cf83
Neil Rackett <neil.rackett@mesmotronic.com>
2016-11-30 16:53:28 +0000

    You can now defer the template load using autoInitTemplate=false and initTemplate()

--- src/conbo/view/View.js
+++ src/conbo/view/View.js
@@ -31,7 +31,8 @@ conbo.View = conbo.Glimpse.extend(
 				'id', 
 				'tagName', 
 				'template', 
-				'templateUrl'
+				'templateUrl',
+				'autoInitTemplate'
 			],
 			
 			// Adds interface properties
@@ -53,32 +54,15 @@ conbo.View = conbo.Glimpse.extend(
  		
 		conbo.makeAllBindable(this, (this.bindable || []).concat(['currentState']));
 		
-		var templateUrl = this.templateUrl
-		  , template = this.template
-		  ;
 		
 		if (this.hasContent)
 		{
 			this.__content =  this.$el.html();
 		}
 		
-		if (!!templateUrl)
+		if (this.autoInitTemplate !== false)
 		{
-			this.loadTemplate(templateUrl);
-		}
-		else
-		{
-			if (conbo.isFunction(template))
-			{
-				template = template(this);
-			}
-			
-			if (conbo.isString(template))
-			{
-				this.$el.html(template);
-			}
-			
-			this.__initView();
+			this.initTemplate();
 		}
 	},
 	
@@ -352,6 +336,36 @@ conbo.View = conbo.Glimpse.extend(
 		conbo.BindingUtils.unbindView(this);
 		this.dispatchEvent(new conbo.ConboEvent(conbo.ConboEvent.UNBOUND));
 		return this;
+	},
+	
+	/**
+	 * Initialize the View's template, either by loading the templateUrl
+	 * or using the contents of the template property, if either exist
+	 */
+	initTemplate: function()
+	{
+		var templateUrl = this.templateUrl
+		  , template = this.template
+		  ;
+		
+		if (!!templateUrl)
+		{
+			this.loadTemplate(templateUrl);
+		}
+		else
+		{
+			if (conbo.isFunction(template))
+			{
+				template = template(this);
+			}
+			
+			if (conbo.isString(template))
+			{
+				this.$el.html(template);
+			}
+			
+			this.__initView();
+		}
 	},
 	
 	/**
