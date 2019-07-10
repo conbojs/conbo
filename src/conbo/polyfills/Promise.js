7b57c752301ddba821825cb2abe2c6ad47a7ddb8
Neil Rackett <neil.rackett@mesmotronic.com>
2019-07-10 14:04:28 +0100

    Added Promise.prototype.finally polyfill

--- src/conbo/polyfills/Promise.js
+++ src/conbo/polyfills/Promise.js
@@ -1,149 +1,294 @@
 /**
- * Lightweight Promise polyfill based on promiscuous
+ * Lightweight Promise polyfill
  */
-if (!window.Promise)
+(function () 
 {
-	(function(func, obj)
+	if (!('Promise' in window))
 	{
-		function is(type, item) { return (typeof item)[0] == type; }
-
-		function Promise(callback, handler) 
+		function Promise(fn) 
 		{
-			handler = function pendingHandler(resolved, rejected, value, queue, then, i) 
+			if (!(this instanceof Promise)) throw new TypeError('Promises must be constructed via new');
+			if (typeof fn !== 'function') throw new TypeError('not a function');
+
+			this._state = 0;
+			this._handled = false;
+			this._value = undefined;
+			this._deferreds = [];
+		
+			doResolve(fn, this);
+		}
+		
+		function handle(self, deferred) 
+		{
+			while (self._state === 3) 
 			{
-				queue = pendingHandler.q;
+				self = self._value;
+			}
 
-				if (resolved != is) 
+			if (self._state === 0) 
+			{
+				self._deferreds.push(deferred);
+				return;
+			}
+
+			self._handled = true;
+
+			setTimeout(function() 
+			{
+				var cb = self._state === 1 ? deferred.onFulfilled : deferred.onRejected;
+
+				if (cb === null) 
 				{
-					return Promise(function(resolve, reject)
-					{
-						queue.push({ p: this, r: resolve, j: reject, 1: resolved, 0: rejected });
-					});
+					(self._state === 1 ? resolve : reject)(deferred.promise, self._value);
+					return;
 				}
 
-				if (value && (is(func, value) | is(obj, value))) 
+				var ret;
+				
+				try 
 				{
-					try { then = value.then; }
-					catch (reason) { rejected = 0; value = reason; }
+					ret = cb(self._value);
+				}
+				catch (e)
+				{
+					reject(deferred.promise, e);
+					return;
 				}
 
-				if (is(func, then)) 
+				resolve(deferred.promise, ret);
+			}, 0);
+		}
+		
+		function resolve(self, newValue) 
+		{
+			try 
+			{
+				if (newValue === self)
 				{
-					try { then.call(value, transferState(1), rejected = transferState(0)); }
-					catch (reason) { rejected(reason); }
+					throw new TypeError('A promise cannot be resolved with itself.');
 				}
-				else 
+
+				if (newValue && (typeof newValue === 'object' || typeof newValue === 'function'))
 				{
-					handler = function(Resolved, Rejected) 
+					var then = newValue.then;
+
+					if (newValue instanceof Promise) 
 					{
-						if (!is(func, (Resolved = rejected ? Resolved : Rejected))) return callback;
-						return Promise(function(resolve, reject) { finalize(this, resolve, reject, value, Resolved); });
-					};
-					i = 0;
-					while (i < queue.length) 
+						self._state = 3;
+						self._value = newValue;
+						finale(self);
+						return;
+					}
+					else if (typeof then === 'function') 
 					{
-						then = queue[i++];
-						if (!is(func, resolved = then[rejected])) (rejected ? then.r : then.j)(value);
-						else finalize(then.p, then.r, then.j, value, resolved);
+						doResolve(conbo.bind(then, newValue), self);
+						return;
 					}
 				}
 
-				function transferState(resolved) 
-				{
-					return function(value) { then && (then = 0, pendingHandler(is, resolved, value)); };
-				}
-			};
-			
-			handler.q = [];
+				self._state = 1;
+				self._value = newValue;
+				finale(self);
+			}
+			catch (e)
+			{
+				reject(self, e);
+			}
+		}
+		
+		function reject(self, newValue) 
+		{
+			self._state = 2;
+			self._value = newValue;
+			finale(self);
+		}
+		
+		function finale(self) 
+		{
+			if (self._state === 2 && self._deferreds.length === 0) 
+			{
+				// Ignore unhandled errors for now
+			}
+		
+			for (var i = 0, len = self._deferreds.length; i < len; i++) 
+			{
+				handle(self, self._deferreds[i]);
+			}
 
-			callback.call(callback = 
-				{
-					then: function(resolved, rejected)
+			self._deferreds = null;
+		}
+		
+		function Handler(onFulfilled, onRejected, promise) 
+		{
+			this.onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : null;
+			this.onRejected = typeof onRejected === 'function' ? onRejected : null;
+			this.promise = promise;
+		}
+		
+		function doResolve(fn, self) 
+		{
+			var done = false;
+
+			try 
+			{
+				fn(
+					function(value) 
 					{
-						return handler(resolved, rejected); 
+						if (done) return;
+						done = true;
+						resolve(self, value);
 					},
-					"catch": function(rejected)
+
+					function(reason) 
 					{
-						return handler(0, rejected); 
-					} 
-				},
-				function(value)
+						if (done) return;
+						done = true;
+						reject(self, reason);
+					}
+				);
+			}
+			catch (ex)
+			{
+				if (done) return;
+				done = true;
+				reject(self, ex);
+			}
+		}
+		
+		Promise.prototype['catch'] = function(onRejected) 
+		{
+			return this.then(null, onRejected);
+		};
+		
+		Promise.prototype.then = function(onFulfilled, onRejected) 
+		{
+			var prom = new this.constructor(conbo.noop);
+		
+			handle(this, new Handler(onFulfilled, onRejected, prom));
+			return prom;
+		};
+				
+		Promise.all = function(arr) 
+		{
+			return new Promise(function(resolve, reject) 
+			{
+				if (!conbo.isArray(arr))
 				{
-					handler(is, 1,	value); 
-				},
-				function(reason)
+					return reject(new TypeError('Promise.all accepts an array'));
+				}
+			
+				var args = Array.prototype.slice.call(arr);
+				if (args.length === 0) return resolve([]);
+				var remaining = args.length;
+			
+				function res(i, val) 
 				{
-					handler(is, 0, reason);
+					try 
+					{
+						if (val && (typeof val === 'object' || typeof val === 'function')) 
+						{
+							var then = val.then;
+
+							if (typeof then === 'function') 
+							{
+								then.call
+								(
+									val,
+									function(val) {
+									res(i, val);
+									},
+									reject
+								);
+
+								return;
+							}
+						}
+
+						args[i] = val;
+
+						if (--remaining === 0) {
+							resolve(args);
+						}
+					}
+					catch (ex) 
+					{
+						reject(ex);
+					}
+				}
+			
+				for (var i = 0; i < args.length; i++) {
+					res(i, args[i]);
+				}
+			});
+		};
+		
+		Promise.resolve = function(value) 
+		{
+			if (value && typeof value === 'object' && value.constructor === Promise) 
+			{
+				return value;
+			}
+		
+			return new Promise(function(resolve) 
+			{
+				resolve(value);
+			});
+		};
+		
+		Promise.reject = function(value) 
+		{
+			return new Promise(function(resolve, reject) 
+			{
+				reject(value);
+			});
+		};
+		
+		Promise.race = function(arr) 
+		{
+			return new Promise(function(resolve, reject) 
+			{
+				if (!conbo.isArray(arr)) 
+				{
+					return reject(new TypeError('Promise.race accepts an array'));
+				}
+			
+				for (var i = 0, len = arr.length; i < len; i++) 
+				{
+					Promise.resolve(arr[i]).then(resolve, reject);
+				}
+			});
+		};
+		
+		window.Promise = Promise;
+	}	
+
+	if (!('finally' in window.Promise.prototype))
+	{
+		window.Promise.prototype['finally'] = function(callback) 
+		{
+			var constructor = this.constructor;
+	
+			return this.then
+			(
+				function(value) 
+				{
+					return constructor.resolve(callback()).then(function() 
+					{
+						return value;
+					});
+				},
+	
+				function(reason) 
+				{
+					return constructor.resolve(callback()).then(function() 
+					{
+						return constructor.reject(reason);
+					});
 				}
 			);
-
-			return callback;
 		}
+	};
 
-		function finalize(promise, resolve, reject, value, transform) 
-		{
-			setImmediate(function()
-			{
-				try 
-				{
-					value = transform(value);
-					transform = value && (is(obj, value) | is(func, value)) && value.then;
-					if (!is(func, transform)) resolve(value);
-					else if (value == promise) reject(TypeError());
-					else transform.call(value, resolve, reject);
-				}
-				catch (error)
-				{
-					reject(error);
-				}
-			});
-		}
-
-		Promise.resolve = ResolvedPromise;
-		
-		function ResolvedPromise(value)
-		{
-			return Promise(function(resolve) { resolve(value); });
-		}
-
-		Promise.reject = function(reason)
-		{
-			return Promise(function(resolve, reject) { reject(reason); }); 
-		};
-
-		Promise.all = function(promises) 
-		{
-			return Promise(function(resolve, reject, count, values) 
-			{
-				values = [];
-				count = promises.length || resolve(values);
-				promises.map(function(promise, index) 
-				{
-					ResolvedPromise(promise).then(
-						function(value) 
-						{
-							values[index] = value;
-							--count || resolve(values);
-						},
-						reject);
-				});
-			});
-		};
-
-		Promise.race = function(promises) 
-		{
-			return Promise(function(resolve, reject) 
-			{
-				promises.map(function(promise) 
-				{
-					ResolvedPromise(promise).then(resolve, reject);
-				});
-			});
-		};
-
-		window.Promise = Promise;
-
-	})('f', 'o');
-}
+})();
 
 conbo.Promise = window.Promise;
